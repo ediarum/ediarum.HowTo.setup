f0615583b7e31dba4a1c7a034d4bf333e82649b6
Philipp Linss <Philipp Linss>
2016-08-30 12:06:16 +0200

    Bisherige Dateien hinzugefügt

---
+++ Linss/Dita_Handbuch Schleiermacher/Menü_Schleiermacher.s
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE concept PUBLIC "-//OASIS//DTD DITA Concept//EN" "concept.dtd">
+<concept id="concept_wmh_ysz_cx">
+ <title>Menü "Schleiermacher"</title>
+ <shortdesc></shortdesc>
+ <conbody>
+  <p>Zusätzlich zu den Werkzeugleisten gibt es auch ein Menü „Schleiermacher“ in der obersten</p>
+  <p>Menüleiste. Hier befinden sich alle Funktionen der Werkzeugleiste sowie einige weitere,</p>
+  <p>die selten benutzt werden</p>
+ </conbody>
+</concept>
